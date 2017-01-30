457970b15f76a68a0fe379c25a29b1d75a9c99c0
Onur BAŞTÜRK <obasturk@anadolu.edu.tr>
2017-01-31 00:33:14 +0200

    iterator design pattern

---
+++ src/com/basturk/iterator/IteratorEverydayDemo.java
@@ -0,0 +1,41 @@
+package com.basturk.iterator;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * Created by basturk
+ * Date: 31.01.2017.
+ */
+public class IteratorEverydayDemo {
+
+    public static void main(String[] args) {
+//        List<String> names = new ArrayList<>();
+
+        Set<String> names = new HashSet<>();
+
+        names.add("Bryan");
+        names.add("Aaron");
+        names.add("Jason");
+
+        Iterator<String> namesItr = names.iterator();
+
+//        for (int i = 0; i < names.size(); i++) {
+//           String name = names.get(i);
+//
+//        }
+
+        while (namesItr.hasNext()) {
+            String name = namesItr.next();
+            System.out.println(name);
+            namesItr.remove();
+        }
+
+//        for (String name : names) {
+//            System.out.println(name);
+//        }
+
+        System.out.println("Names size: " + names.size());
+    }
+}
